2ae8195e28355904a868ddc2e429f0d66b078727
JoeSoellner <jsoe08@gmail.com>
2021-01-15 16:12:09 -0600

    update blog component

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,41 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-	<div>
-		{blog.title} {blog.author}
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+	const blogStyle = {
+	  paddingTop: 10,
+	  paddingLeft: 2,
+	  border: 'solid',
+	  borderWidth: 1,
+	  marginBottom: 5
+	}
+
+	const [detailsVisible, setDetailsVisible] = useState(false)
+
+	const showDetails = (visible) => {
+		if(visible) {
+			return (
+				<div>
+					{blog.author} <br />
+					{blog.url} <br />
+					{'likes: '} {blog.likes}
+					<button type='submit'>like</button>
+				</div>
+			)
+		} else {
+			return (<></>)
+		}
+	}
+	
+	return (
+	  <div style={blogStyle}>
+		<div>
+		  	{blog.title}
+		  	<button onClick={() => setDetailsVisible(!detailsVisible)} type='button'>
+				{detailsVisible ? 'hide' : 'details'}
+			</button>
+		</div>
+		{showDetails(detailsVisible)}
 	</div>
-)
+  )}
 
 export default Blog
